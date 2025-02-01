c5381bcdf0b9be8d257df154af367afdaed136a7
masihgh <masihgraph@gmail.com>
2025-02-01 04:44:19 +0330

    change pages + init tailwindcss nuxt

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+   <h1>Welcome to Tehran Metro Vue</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  tailwindcss: {
+    cssPath: ["~/assets/css/tailwind.css", { injectPosition: "first" }],
+    config: {},
+    viewer: true,
+    exposeConfig: false,
+  },
+});
