ecbd02704230efc7dc2adef81b7655d8d3f72aaa
masihgh <masihgraph@gmail.com>
2025-02-01 04:53:19 +0330

    first darkmode in tailwindcss and vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import colors from 'tailwindcss/colors'
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    '@nuxtjs/color-mode'
+  ],
   tailwindcss: {
     cssPath: ["~/assets/css/tailwind.css", { injectPosition: "first" }],
-    config: {},
+    config: {
+      theme: {
+        extend: {
+          colors: { primary: colors.green }
+        }
+      }
+    },
     viewer: true,
     exposeConfig: false,
+    editorSupport: true,
   },
 });
